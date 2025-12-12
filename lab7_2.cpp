daa6c80909693113afd1c440e21e43ebf81d03f8
PaoPei01 <paopei2706@gmail.com>
2025-12-12 16:57:36 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,31 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main()
+{
+    string Name, Movie, Day;
+    int ID, Find12, Gear;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin, Name);
+    cout << "Fahsai: Wow!!! " << Name << " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << Name << ": ";
+    cin >> ID;
+    Find12 = ID/10000000;
+    Gear = Find12 - 12;
+    cout << "Fahsai: I think you may be GEAR " << Gear << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << Name << ": ";
+    cin.ignore();
+    getline(cin, Movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << Name << ": ";
+    getline(cin, Day);
+    cout << "Fahsai: " << Day << "....that is OK!!! I'm looking forward to watching " << Movie << " with you.\n";
+    cout << Name << ": ";
+    cout << "Fahsai: 555+ see you " << Day << ". Bye Bye \\(^ ^)/";  
+return 0;
+}
